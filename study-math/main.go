f9e0a87133a6bce275cd3c061be256ac042c6ebc
bidianqing <bidianqing@longfor.com>
2024-03-04 15:58:30 +0800

    *

---
+++ study-math/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// 向上取整 天花板函数
+	fmt.Println(math.Ceil(9.1))
+
+	// 向下取整 地板函数
+	fmt.Println(math.Floor(3.9))
+
+	// 绝对值
+	fmt.Println(math.Abs(-10))
+}
