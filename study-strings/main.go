1e4b257d35f414e5242c417b05b2fd488c5b4ea7
bidianqing <bidianqing@longfor.com>
2024-03-04 15:47:28 +0800

    *

---
+++ study-strings/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// 去除字符长前面和后面的空格
+	// name := "    tom        "
+	// fmt.Println(strings.Trim(name, " "))
+
+	// 判断是否以指定字符串开头并去除
+	token := "Bearer 123456"
+	fmt.Println(strings.HasPrefix(token, "Bearer "))
+	fmt.Println(strings.TrimPrefix(token, "Bearer "))
+}
